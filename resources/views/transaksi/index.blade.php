7dc20f9af17e686f31251a5ab0e84b7ac71327d3
Muchamad Faza Fadillah <fadilahfaza43@gmail.com>
2024-03-21 10:41:14 +0700

    "Mengubah div card pada pemesanan"

--- resources/views/transaksi/index.blade.php
+++ resources/views/transaksi/index.blade.php
@@ -162,6 +162,16 @@
         .btn-inc {
             margin: 10px;
         }
+
+        .card-body h6 {
+            cursor: pointer;
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            right: 0;
+            left: 0;
+            padding-top: 130px;
+        }
     </style>
 @endpush
 
